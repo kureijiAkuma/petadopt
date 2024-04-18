04571d7f1a56a6ccee8115f15c2e51126249c974
Ciboow <120304483+Ciboow@users.noreply.github.com>
2024-04-18 11:51:57 +0800

    Add files via upload

--- src/Dashboard/UpdateShopItem.jsx
+++ src/Dashboard/UpdateShopItem.jsx
@@ -249,7 +249,7 @@ const ProductTable = () => {
                   value={editedData.name}
                   onChange={handleInputChange}
                   className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  readOnly
+                  
                 />
               </div>
               <div className="mb-4">
